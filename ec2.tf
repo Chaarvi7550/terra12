094c958279cd1beb145eaefda2c6845dc08dcca4
usharanigudala707 <132543970+usharanigudala707@users.noreply.github.com>
2023-06-19 12:24:02 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -9,12 +9,12 @@ terraform {
 
 # Configure the AWS Provider
 provider "aws" {
-  region = "ap-south-1"
+  region = "us-east-1b"
 }
 
 resource "aws_instance" "web" {
   ami           = "ami-0f5ee92e2d63afc18"
-  instance_type = "t3.micro"
+  instance_type = "t2.micro"
 
   tags = {
     Name = "HelloWorld"
